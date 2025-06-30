7913359c6d07aee19ceeabe6eaf5ba1c65ed5894
Antonio Rivas <antonio.rivas@fortris.com>
2025-06-30 21:26:00 +0200

    Favorites route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { GameCatalogueDetail } from './components/organism/GameCatalogueDetail';
 import { ContainerGameCatalogueListComponent } from './components/pages/ContainerGameCatalogueList/ContainerGameCatalogueList.component';
+import { GameCatalogueFavoriteList } from './components/organism/GameCatalogueFavoriteList';
+import { TopBar } from './components/molecules/TopBar/TopBar';
 import './App.css';
 
 const queryClient = new QueryClient();
@@ -11,9 +13,11 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+        <TopBar />
         <Routes>
           <Route path="/" element={<ContainerGameCatalogueListComponent />} />
           <Route path="/game/:id" element={<GameCatalogueDetail />} />
+          <Route path="/favorites" element={<GameCatalogueFavoriteList />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
